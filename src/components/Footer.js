e4ab3531efc866b9740a11a12c4229ca461890ae
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-02 15:31:09 +0530

    Add social media links.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,14 @@ const Footer = () => {
         <p className="copyright">Copyright 2020 Adarsh Nanwani</p>
         <ul className="social">
           <li>
-            <a href="">Github</a>
+            <a href="https://github.com/adarshnanwani" target="_blank">
+              Github
+            </a>
           </li>
           <li>
-            <a href="">Twitter</a>
+            <a href="https://twitter.com/AdyNanwani" target="_blank">
+              Twitter
+            </a>
           </li>
         </ul>
       </div>
